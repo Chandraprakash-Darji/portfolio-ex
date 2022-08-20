562548b8528fb4978ca425da46c7b4b982739dec
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-21 00:07:35 +0530

    Added Tooltip to the Tools Icon and scale down image on hover

---
+++ src/components/About/chunk/ToolIcon.tsx
@@ -0,0 +1,24 @@
+type ToolIconProps = {
+    name: string;
+    path: string;
+};
+
+const ToolIcon = ({ name, path }: ToolIconProps) => {
+    return (
+        <li
+            className="bg-primary/20 p-4 hover:bg-main/5 flex justify-center items-center rounded-xl border border-primary/70 hover:border-primary/50 shadow-md shadow-main/20 hover:shadow-none hover:rounded-[4rem] transition-all duration-300 group relative"
+            title={name}
+        >
+            <img
+                className="w-14 md:w-20 group-hover:p-5 aspect-auto transition-all duration-300 "
+                src={path}
+                alt={name}
+                draggable="false"
+            />
+            <div className="absolute top-3/4 whitespace-nowrap bg-primary text-dark p-0 group-hover:px-2 group-hover:py-1 z-20 rounded-lg transition-all duration-300 scale-0 group-hover:scale-100 overflow-hidden      group-hover:opacity-100">
+                {name}
+            </div>
+        </li>
+    );
+};
+export default ToolIcon;
