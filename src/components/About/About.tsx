51a5b1fde43a13758a20d9c555b77a29ac3db39a
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-21 17:32:02 +0530

    Added small line below About section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,28 +1,31 @@
-import { InfoSection, LangaugeAndTools } from ".";
+import { BelowLine, InfoSection, LangaugeAndTools } from ".";
 import useElementOnSecreen from "../../hook/useElementOnSecreen";
 import AnimatedText from "../AnimatedText";
 import Heading2 from "../Dummy/Heading2";
-
+import './About.css'
 const About = () => {
     const [isVisible, containerRef] = useElementOnSecreen();
 
     return (
         <section
+        id="aboutme"
             ref={containerRef}
-            id="aboutme"
-            className={`container mx-auto md:mt-36 xl:mt-72 transition-all duration-500 ease-linear ${
+            className={`container mx-auto md:mt-36 xl:mt-72 transition-all duration-500 ease-linear relative ${
                 isVisible ? "translate-x-0" : "opacity-0 translate-y-36"
             }`}
         >
             <div className="flex justify-center items-center">
                 <Heading2>
-                    {isVisible ? <AnimatedText text="AboutMe" /> : ""}
+                    {isVisible ? <AnimatedText text="Something About Me" /> : ""}
                     {/* AboutMe */}
                 </Heading2>
             </div>
             <div className="flex flex-col lg:flex-row mt-10 gap-x-10 xl:gap-x-16 gap-y-10">
                 <InfoSection />
                 <LangaugeAndTools />
+            </div>
+            <div className="absolute -bottom-48 left-1/2 -translate-x-1/2 ">
+                <BelowLine />
             </div>
         </section>
     );
--- src/components/About/index.ts
+++ src/components/About/index.ts
@@ -2,4 +2,5 @@ import About from "./About";
 export { default as InfoSection } from "./chunk/InfoSection";
 export { default as ToolIcon } from "./chunk/ToolIcon";
 export { default as LangaugeAndTools } from "./chunk/LangaugeAndTools";
+export { default as BelowLine } from "./chunk/BelowLine";
 export default About;
