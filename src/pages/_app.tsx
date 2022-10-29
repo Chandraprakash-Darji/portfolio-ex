f345591731717aca9d766af10e5f2bb71f3136f7
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-10-30 03:27:24 +0530

    removes invalid semi colon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
