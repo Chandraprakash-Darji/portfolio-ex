9e98be3e8e0ec358bb60fbe28836ffb7e1d3ae98
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-19 09:51:39 +0530

    feat: add Comp. NextImage, Seo, Skeleton

---
+++ src/components/Seo.tsx
@@ -0,0 +1,97 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const defaultMeta = {
+  title: 'Chandraprakash Darji - Frontend Developer',
+  siteName: 'Chandraprakash Darji',
+  description:
+    'I am Chandraprakash Darji. Frontend developer Based in India. Like Play with Css. Tech Stack are Html, CSS, JS, TS, React, NEXT.JS, Tailwindcss, Scss.',
+  url: 'chandraprakash.codes',
+  type: 'website',
+  robots: 'follow, index',
+  image: '/images/large-og.png',
+};
+
+type SeoProps = {
+  date?: string;
+  templateTitle?: string;
+} & Partial<typeof defaultMeta>;
+
+export default function Seo(props: SeoProps) {
+  const router = useRouter();
+  const meta = {
+    ...defaultMeta,
+    ...props,
+  };
+  meta['title'] = props.templateTitle
+    ? `${props.templateTitle} | ${meta.siteName}`
+    : meta.title;
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name='robots' content={meta.robots} />
+      <meta content={meta.description} name='description' />
+      <meta property='og:url' content={`${meta.url}${router.asPath}`} />
+      <link rel='canonical' href={`${meta.url}${router.asPath}`} />
+      {/* Open Graph */}
+      <meta property='og:type' content={meta.type} />
+      <meta property='og:site_name' content={meta.siteName} />
+      <meta property='og:description' content={meta.description} />
+      <meta property='og:title' content={meta.title} />
+      <meta name='image' property='og:image' content={meta.image} />
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@chandra_7852' />
+      <meta name='twitter:title' content={meta.title} />
+      <meta name='twitter:description' content={meta.description} />
+      <meta name='twitter:image' content={meta.image} />
+      {meta.date && (
+        <>
+          <meta property='article:published_time' content={meta.date} />
+          <meta
+            name='publish_date'
+            property='og:publish_date'
+            content={meta.date}
+          />
+          <meta name='author' property='article:author' content='MarkitUp' />
+        </>
+      )}
+
+      {/* Favicons */}
+      {favicons.map((linkProps) => (
+        <link key={linkProps.href} {...linkProps} />
+      ))}
+      <meta name='msapplication-TileColor' content='#fea55f' />
+      <meta name='msapplication-config' content='/favicon/browserconfig.xml' />
+      <meta name='theme-color' content='#fea55f' />
+    </Head>
+  );
+}
+
+const favicons: Array<React.ComponentPropsWithoutRef<'link'>> = [
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/favicon/apple-touch-icon.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon/favicon-16x16.png',
+  },
+  { rel: 'manifest', href: '/favicon/site.webmanifest' },
+  {
+    rel: 'mask-icon',
+    href: '/favicon/safari-pinned-tab.png',
+    color: '#fea55f',
+  },
+  { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+];
