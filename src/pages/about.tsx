6ce3925937fbbbb5683fc9bc16877315d6ea8e03
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-19 10:24:11 +0530

    feat: init about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import Seo from '@/components/Seo';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Seo templateTitle='About the' />
+
+      <section className=''>
+        <div className=''></div>
+      </section>
+    </>
+  );
+}
