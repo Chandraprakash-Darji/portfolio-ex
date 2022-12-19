8b9b6b9807bffe9d4597402aba5a996dabddd8d0
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-19 10:07:29 +0530

    feat: add Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,49 @@
-import { NextPage } from 'next';
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { About, Contact, Hero, Projects } from '../components/pages/Home';
-import SecModal from '../components/pages/Home/15SecModal';
-import Navbar from '../components/widget/Navbar';
+import NextImage from '@/components/NextImage';
+import Seo from '@/components/Seo';
 
-const Landing: NextPage = () => {
-  const [attention, setAttention] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('ds');
-      setAttention(true);
-    }, 15000);
-  }, []);
-
+export default function HomePage() {
   return (
-    <div className="relative min-h-screen overflow-x-hidden font-mono">
-      <Head>
-        <title>Portfolio - Chandraprakash Darji</title>
-      </Head>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-      {attention && <SecModal handleClose={() => setAttention(false)} />}
-    </div>
-  );
-};
+    <>
+      <Seo templateTitle='Home' />
 
-export default Landing;
+      <section className='layout flex h-full items-center justify-center gap-10'>
+        <div className='flex-1'>
+          <h1>
+            <span className='h4 text-white'>
+              Hi all. I am <br />
+            </span>
+            <span className='h1 text-white'>Chandra Prakash</span>
+            <br />
+            <span className='h2 text-purple'>&gt; Frontend Developer</span>
+          </h1>
+          <p className='mt-12'>&#47;&#47; complete the game to continue</p>
+          <p className='mt-1'>
+            &#47;&#47; you can also see it on my Github page
+          </p>
+          <p className='mt-1 text-white'>
+            <span className='text-purple'>const</span>{' '}
+            <span className='text-green'>githubLink</span> =
+            <span className='text-orange'>
+              {' '}
+              “https://github.com/Chandraprakash-Darji/chandraprakash.codes”
+            </span>
+          </p>
+        </div>
+        <div className='relative'>
+          <NextImage
+            className='absolute top-1/2 left-1/2 aspect-[1114/1030] w-[60rem] -translate-x-1/2 -translate-y-1/2'
+            fill
+            src='/images/hero/backblur.svg'
+            alt='snake'
+          />
+          <NextImage
+            className='relative aspect-[510/475] w-[30rem]'
+            fill
+            src='/images/hero/snake.png'
+            alt='snake'
+          />
+        </div>
+      </section>
+    </>
+  );
+}
