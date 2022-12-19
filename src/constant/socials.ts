96ec82c6c3b7f93821d920e585ca86cf4c3e07e0
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-19 09:55:21 +0530

    chore(contact): add social list

---
+++ src/constant/socials.ts
@@ -0,0 +1,10 @@
+import { SocialNameType } from '@/lib/types';
+
+const socials: Record<SocialNameType, string> = {
+  github: 'https://github.com/Chandraprakash-Darji',
+  linkedin: 'https://www.linkedin.com/in/Chandraprakash-Darji',
+  twitter: 'https://twitter.com/chandra_7852',
+  mail: 'mailto:prakashchandra3786@gmail.com',
+  instagram: 'https://www.instagram.com/chandraprakash_7852/',
+};
+export default socials;
