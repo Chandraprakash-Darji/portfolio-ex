47fd4d56f996b3700283bd0280e915523f5978f1
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-20 20:10:58 +0530

    chore(socials icon): change path of social Icons to Icons/social

--- src/Icons/index.ts
+++
@@ -1,7 +0,0 @@
-import GithubIcon from '@/Icons/GithubIcon';
-import InstagramIcon from '@/Icons/InstagramIcon';
-import LinkedInIcon from '@/Icons/LinkedInIcon';
-import MailIcon from '@/Icons/MailIcon';
-import TwitterIcon from '@/Icons/TwitterIcon';
-
-export { GithubIcon, InstagramIcon, LinkedInIcon, MailIcon, TwitterIcon };
---
+++ src/Icons/socials/index.ts
@@ -0,0 +1,7 @@
+import GithubIcon from '@/Icons/socials/GithubIcon';
+import InstagramIcon from '@/Icons/socials/InstagramIcon';
+import LinkedInIcon from '@/Icons/socials/LinkedInIcon';
+import MailIcon from '@/Icons/socials/MailIcon';
+import TwitterIcon from '@/Icons/socials/TwitterIcon';
+
+export { GithubIcon, InstagramIcon, LinkedInIcon, MailIcon, TwitterIcon };
