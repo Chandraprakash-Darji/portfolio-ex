8c17883739b88862d67ae74f9cda27329815a999
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-20 21:22:07 +0530

    refactor: add menu and activity icon export file

---
+++ src/Icons/sidebar/activity/index.ts
@@ -0,0 +1,5 @@
+import HobbiesIcon from '@/Icons/sidebar/activity/HobbiesIcon';
+import PersonalIcon from '@/Icons/sidebar/activity/PersonalIcon';
+import ProfesstionalIcon from '@/Icons/sidebar/activity/ProfesstionalIcon';
+
+export { HobbiesIcon, PersonalIcon, ProfesstionalIcon };
---
+++ src/Icons/sidebar/menu/index.ts
@@ -0,0 +1,5 @@
+import ArrowIcon from '@/Icons/sidebar/menu/ArrowIcon';
+import FilledArrowIcon from '@/Icons/sidebar/menu/FilledArrowIcon';
+import FolderIcon from '@/Icons/sidebar/menu/FolderIcon';
+import MarkdownIcon from '@/Icons/sidebar/menu/MarkdownIcon';
+export { ArrowIcon, FilledArrowIcon, FolderIcon, MarkdownIcon };
