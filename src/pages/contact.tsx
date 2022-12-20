5d5bed3ca22d39ff99f948d6186e33698d46b962
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-20 21:19:13 +0530

    feat: add empty contact and projects page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Seo from '@/components/Seo';
+
+export default function ContactPage() {
+  return (
+    <>
+      <Seo templateTitle='Contact' />
+
+      <section className=''>
+        <div className=''></div>
+      </section>
+    </>
+  );
+}
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Seo from '@/components/Seo';
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <Seo templateTitle='Projects of' />
+
+      <section className=''>
+        <div className=''></div>
+      </section>
+    </>
+  );
+}
