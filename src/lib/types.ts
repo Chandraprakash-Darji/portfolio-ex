c5669a8c4a998678210bdcc568745fa646cb8cf5
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-20 21:28:20 +0530

    chore: decalred types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,30 @@ export type SocialNameType =
   | 'mail'
   | 'instagram'
   | 'linkedin';
+
+// used for the icon to use
+export type FileExtType = 'folder' | 'markdown';
+// Color that going to used for folder
+export type FolderColorType = 'orange' | 'green' | 'purple';
+
+// Base type for file and folder
+export type BaseFileType = {
+  name: string;
+  icon: FileExtType;
+};
+
+// File type
+export type FileType = BaseFileType & {
+  href: string;
+};
+
+// Folder type
+export type FolderType = BaseFileType & {
+  color: FolderColorType;
+  children: Array<FileType | FolderType>;
+};
+
+// Menu type
+export type menuType = BaseFileType & {
+  children: Array<FileType | FolderType>;
+};
