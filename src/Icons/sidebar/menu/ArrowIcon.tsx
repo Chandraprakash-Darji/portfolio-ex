14245e2d874df3922367ae5ca95eb6a5fab1e33b
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-12-20 20:29:09 +0530

    feat(Icons): add Icons for sidebar

---
+++ src/Icons/sidebar/menu/ArrowIcon.tsx
@@ -0,0 +1,18 @@
+const ArrowIcon = () => {
+  return (
+    <svg
+      width='13'
+      height='9'
+      viewBox='0 0 13 9'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M6.364 5.27715L11.314 0.327148L12.728 1.74115L6.364 8.10515L0 1.74115L1.414 0.327148L6.364 5.27715Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+};
+
+export default ArrowIcon;
---
+++ src/Icons/sidebar/menu/FilledArrowIcon.tsx
@@ -0,0 +1,18 @@
+const FilledArrowIcon = () => {
+  return (
+    <svg
+      width='9'
+      height='7'
+      viewBox='0 0 9 7'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M4.49657 6.51172L0.246551 0.511719L8.74658 0.511719L4.49657 6.51172Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+};
+
+export default FilledArrowIcon;
