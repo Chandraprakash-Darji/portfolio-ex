cb16ce4a76233d06e88655d190a4c61763a417e5
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2023-04-26 05:44:50 +0530

    chore: tools sequence update

--- src/lib/data/AppIcons.ts
+++ src/lib/data/AppIcons.ts
@@ -1,9 +1,9 @@
 const AppIcons = [
-  { path: '/assets/icons/javascript.svg', name: 'Javascript', key: 1 },
-  { path: '/assets/icons/react.svg', name: 'React JS', key: 2 },
   { path: '/assets/icons/typescript.svg', name: 'Typescript', key: 3 },
-  { path: '/assets/icons/tailwindcss.svg', name: 'TailwindCSS', key: 4 },
   { path: '/assets/icons/nextjs.svg', name: 'Next JS', key: 5 },
+  { path: '/assets/icons/react.svg', name: 'React JS', key: 2 },
+  { path: '/assets/icons/tailwindcss.svg', name: 'TailwindCSS', key: 4 },
+  { path: '/assets/icons/javascript.svg', name: 'Javascript', key: 1 },
   { path: '/assets/icons/sass.svg', name: 'SASS', key: 6 },
   { path: '/assets/icons/nodejs.svg', name: 'Node JS', key: 7 },
   { path: '/assets/icons/figma.svg', name: 'Figma', key: 8 },
